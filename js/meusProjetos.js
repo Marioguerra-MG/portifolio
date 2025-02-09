e615692a27d9e688c1debdc9c9adc8ea35bb4f18
Mario <marioguerradasilvaneto93@gmail.com>
2025-02-09 20:18:02 -0300

    portifolio

---
+++ js/meusProjetos.js
@@ -0,0 +1,15 @@
+function scrollParaProjetos(){
+    document.getElementById("conteudo").scrollIntoView({behavior: "smooth"});
+}
+
+function scrollParaHabilidades(){
+    document.getElementById("habilidades").scrollIntoView({behavior: "smooth"});
+}
+
+function scrollParaSobre(){
+    document.getElementById("sobreMin").scrollIntoView({behavior: "smooth"});
+}
+
+function scrollParaContatos(){
+    document.getElementById("contatos").scrollIntoView({behavior: "smooth"});
+}
